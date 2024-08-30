13f1236e0114ea39a07f73fee27bcddcfd5afdcb
panudet ingai <codedroy3@gmail.com>
2024-08-30 11:49:52 +0700

    chore: Update API route to include switch button data

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -14,9 +14,9 @@ const client = new Client({
 client.connect();
 export async function POST(req: Request) {
     try {
-        const { distance, duty_cycle, last_switch_state } = await req.json();
+        const { distance, duty_cycle, switchbutton} = await req.json();
         // Hash password
-        const res = await client.query('INSERT INTO "tbl_Panudetingai020" (ultrasonic_value, buzzer_value, last_switch_state) VALUES ($1, $2, $3) RETURNING *', [distance, duty_cycle, last_switch_state]);
+        const res = await client.query('INSERT INTO "tbl_Panudetingai020" (ultrasonic_value, buzzer_value, last_switch_state) VALUES ($1, $2, $3) RETURNING *', [distance, duty_cycle, switchbutton]);
         return new Response(JSON.stringify(res.rows[0]), {
             status: 201,
             headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
